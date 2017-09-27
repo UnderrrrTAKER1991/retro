73644b29bac983fe2b5eb813ad0d366d1085747b
Undertaker <branskiy@opu.ua>
2017-09-27 19:33:59 +0300

    first commit

---
+++ dist/assets/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+$(document).ready(function () {
+    $('.slider').slick({
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+});
+/*
+$(".mobile-button").click(function(){
+    $('.menu').addClass('menu-open');
+    $(".abs-black").fadeIn(300);
+});
+
+$('.menu-close,.abs-black').click(function(){
+    $('.menu').removeClass('menu-open');
+    $(".abs-black").fadeOut(300);
+});
+*/
---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+});
+/*
+$(".mobile-button").click(function(){
+    $('.menu').addClass('menu-open');
+    $(".abs-black").fadeIn(300);
+});
+
+$('.menu-close,.abs-black').click(function(){
+    $('.menu').removeClass('menu-open');
+    $(".abs-black").fadeOut(300);
+});
+*/
